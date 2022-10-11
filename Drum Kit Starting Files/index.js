65912093169ceb80d28ca20dc19aae1cecdb36c8
MichaelReid85 <Mike1109r@yahoo.com>
2022-10-11 12:14:23 -0700

    DOM drumkit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,9 +3,20 @@
 
 for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
 
+
 document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-  alert("I got clicked!");
-  //What to do when click detected.
+
+
+
+console.log(this.style.color = "white");
+
+
 });
 
 }
+
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
