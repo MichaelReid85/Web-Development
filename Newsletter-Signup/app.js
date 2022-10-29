fcbf293c2b4e245fcd2946c6cd5a2b8f8f7062e5
MichaelReid85 <Mike1109r@yahoo.com>
2022-10-29 14:27:38 -0700

    newsletter update with heroku

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -41,10 +41,17 @@ app.post("/", function(req, res) {
 
   const options = {
     method: "POST",
-    auth: "mike1:2ce83825b7e2ec6a1fcb1a8c3a047515-us21"
+    auth: "mike1:2ce83825b7e2ec6a1fcb1a8c3a047515-us21 "
   }
 
   const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function(data){
       console.log(JSON.parse(data));
     })
@@ -55,7 +62,12 @@ app.post("/", function(req, res) {
 
 });
 
-app.listen(3000, function() {
+app.post("/failure", function(req, res) {
+  res.redirect("/")
+})
+
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000.");
 })
 
