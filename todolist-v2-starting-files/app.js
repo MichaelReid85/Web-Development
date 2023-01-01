941196cf02401c79ffc9a7cd42efcde23dea5447
MichaelReid85 <Mike1109r@yahoo.com>
2023-01-01 12:17:37 -0800

    blog challenge - section 30

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-mike:Test123@cluster0.iaz95od.mongodb.net/todolistDB", {
   useNewUrlParser: true
 });
 
