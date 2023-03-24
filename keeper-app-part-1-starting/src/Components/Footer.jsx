ea4556b22ea695efbcff5ce4620fe5067deab85d
MichaelReid85 <Mike1109r@yahoo.com>
2023-03-24 11:34:11 -0700

    start up project

---
+++ keeper-app-part-1-starting/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+    const currentYear = new Date().getFullYear();
+
+    return ( <
+        div className = "footer" >
+        <
+        p className = "footer p" > Copyright {
+            currentYear
+        } < /p> <
+        /div>
+    );
+}
+
+export default Footer;
---
+++ keeper-app-part-1-starting/src/Components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+    return ( <
+        div className = "note" >
+        <
+        h1 id = "note h1" > Note Title < /h1> <
+        p id = "note p" > Note content < /p> <
+        /div>
+    );
+}
+
+export default Note;
